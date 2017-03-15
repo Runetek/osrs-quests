bd23ddb2b61c3aa644ab49bc74198072cc8bd513
Kyle Stevenson <kyle@kylestevenson.me>
2017-03-15 01:39:33 -0700

    Rename Ye -> Random

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Explorer from '@/components/Explorer'
-import Ye from '@/components/Ye'
+import Random from '@/components/Random'
 
 Vue.use(Router)
 
@@ -10,7 +10,7 @@ export default new Router({
     {
       path: '/',
       name: 'Home',
-      component: Ye
+      component: Random
     },
     {
       path: '/quest/:slug',
